b3dd855dadc00f3f70bc41802144bc075f375d35
liziliang <liziliangluck@163.com>
2024-07-09 20:24:22 +0800

    register datafeed

--- vnpy_ctabacktester/engine.py
+++ vnpy_ctabacktester/engine.py
@@ -57,6 +57,10 @@ class BacktesterEngine(BaseEngine):
         # Optimization result
         self.result_values: list = None
 
+    def register_datafeed(self, datafeed: BaseDatafeed):
+        self.datafeed = datafeed
+        self.write_log("Datafeed registered!")
+
     def init_engine(self) -> None:
         """"""
         self.write_log("初始化CTA回测引擎")
